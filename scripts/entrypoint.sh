2479e07cfdd8878d273df868a2abb801862bc5c9
Rick Bennett <rbennett1106@gmail.com>
2021-08-15 17:07:43 -0400

    feat: add laravel caching if environment is not local
    This will cache the application config, as well as the application routes when not running the app in a local envronment.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Optimize Laravel if the environemnt is not local.
-# if [ -f "artisan" ] && [ ! -z "$APP_ENV" ] && [ "$APP_ENV" != "local" ]; then
-#   php artisan config:cache
-#   php artisan route:cache
-# fi
+if [ -f "artisan" ] && [ ! -z "$APP_ENV" ] && [ "$APP_ENV" != "local" ]; then
+  php artisan config:cache
+  php artisan route:cache
+fi
 
 # Run Supervisor which will start the applications.
 /usr/bin/supervisord -c /etc/supervisord/supervisord.conf
